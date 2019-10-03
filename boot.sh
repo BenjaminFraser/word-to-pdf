a2bb1bc75aa892516ac9264ff22706379d7eb613
BENJAMINFRASER <Ben-fraser@hotmail.co.uk>
2019-10-03 20:25:36 +0300

    Fixed boot.sh error that would result in an infinite loop and prevent running of the application.

--- boot.sh
+++ boot.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-while true; do
-    flask db upgrade
-    if [[ "$?" == "0" ]]; then
-        break
-    fi
-    echo Upgrade command failed, retrying in 5 secs...
-    sleep 5
-done
+flask db upgrade
 exec gunicorn -b :5000 --access-logfile - --error-logfile - wordtopdf:app
